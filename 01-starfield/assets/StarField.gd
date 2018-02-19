36ea9adf48ec4b9b8ccd1a5854590c6f77374c37
David Rubert <david.rubert@gmail.com>
2018-02-19 08:37:53 +0100

    update

--- 01-starfield/assets/StarField.gd
+++ 01-starfield/assets/StarField.gd
@@ -8,13 +8,14 @@ var spread = 256
 var starNode = load("res://Star.tscn")
 var stars = []
 
-func SetPos():
+func SetPos(drawsize):
 	randomize()
-	return Vector3(rand_range(-1000, 1000), rand_range(-1000, 1000), rand_range(200, 255))
+	return Vector3(rand_range(-drawsize.x, drawsize.x), rand_range(-drawsize.y, drawsize.y), rand_range(200, 255))
 
 func _ready():
+	var drawsize = OS.get_window_size()
 	for n in range(num_stars):
-		starfield.append(SetPos())
+		starfield.append(SetPos(drawsize))
 		stars.append(starNode.instance())
 		add_child(stars[n])
 	
@@ -22,17 +23,17 @@ func _ready():
 func DrawStar(n, drawsize):
 
 	if starfield[n].z < speed:
-		starfield[n] = SetPos()
+		starfield[n] = SetPos(drawsize)
 		
 	starfield[n].z -= speed
 	var sx = (starfield[n].x*spread)/(starfield[n].z)+drawsize.x/2
 	var sy = (starfield[n].y*spread)/(starfield[n].z)+drawsize.y/2
 	
 	if sx < 0 or sx > drawsize.x:
-		starfield[n] = SetPos()
+		starfield[n] = SetPos(drawsize)
 	
 	if sy < 0 or sy > drawsize.y:
-		starfield[n] = SetPos()
+		starfield[n] = SetPos(drawsize)
 	
 	var size = Vector2(1-starfield[n].z*0.004, 1-starfield[n].z*0.004)
 	stars[n].set_position(Vector2(sx, sy))
